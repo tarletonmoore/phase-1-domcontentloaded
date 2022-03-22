d7559b196ea723fd3e25c9e77b8d9a2ac89fc0ca
John Tarleton Moore <tarletonmoore@gmail.com>
2022-03-22 13:28:01 -0400

    commit tree d228d107a3ec808dbc8a7b7a9a29832e14d4f97f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    let p = document.querySelector(`p`)
+    p.textContent = "This is really cool!"
+});
+
+// function DOMContentLoaded() {
+//     let p = document.querySelector(`p`)
+//     p.textContent = "This is really cool!"
+// }
+// DOMContentLoaded()
